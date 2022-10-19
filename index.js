ab20a2c1b6b484aca5d554e8a3f1c6ce3858d122
rgwebfront <116201869+rgwebfront@users.noreply.github.com>
2022-10-19 21:05:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var operationButtonFunction = document.getElementsByClassName("operationButtons")
+
+var input1 = document.getElementById('number1');
+var input2 = document.getElementById('number2');
+
+
+function makeOperation(operationCode) {
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
+    if (operationCode === '+') {
+        var result = number1 + number2;
+    } else if (operationCode === '-') {
+        var result = number1 - number2;
+    } else if (operationCode === '*') {
+        var result = number1 * number2;
+    } else if (operationCode === '/') {
+        var result = number1 / number2;
+    } else {
+        window.alert('operation is unknown');
+    }
+    window.alert(result);
+}
+
+function onOperationButtonClick(eventObject) {
+    var clickedElement = eventObject.currentTarget;
+    var operation = clickedElement.innerHTML;
+    makeOperation(operation);
+}
+
+for (var i = 0; i < operationButtonFunction.length; i++) {
+    var button = operationButtonFunction[i];
+    button.addEventListener('click', onOperationButtonClick);
+
+}
+
